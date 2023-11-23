97bad8cb273b4cbde486fbc9b9257c29888490c8
DESKTOP-J2R3IU9\Papie <arekrasztar@gmail.com>
2023-11-23 17:53:27 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace E_Restauracja
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseEventArgs e) 
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void LogIn_Button(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("https://localhost:7028/");
+
+
+                var responseTask = httpClient.GetAsync($"api/Values/{LoginBox.Text}?Haslo={PasswordBox.Password}");
+
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    var rola = readTask.Result;
+                    TestBox.Text = rola; 
+
+                    if(rola == "manager")
+                    {
+
+                        ManagerMenu managerMenu = new ManagerMenu();
+                        this.Close();
+                        managerMenu.ShowDialog();
+                        
+                       
+                       
+                    }
+                    else
+                    {
+                        if(rola == "kucharz")
+                        {
+                            
+                        }
+                    }
+
+                            
+                   
+                }
+
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
